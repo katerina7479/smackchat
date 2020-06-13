25e3f07fc0e0d875630beb1a808bdcdb48f1d6b8
Katerina Hanson <katerina.hanson@gmail.com>
2020-06-13 15:09:42 -0700

    Random avatar picker

--- app/src/main/java/com/katerinah/smackchat/CreateUserActivity.kt
+++ app/src/main/java/com/katerinah/smackchat/CreateUserActivity.kt
@@ -3,15 +3,30 @@ package com.katerinah.smackchat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
     fun onGenerateAvatarColor(view: View){
+        val random  = Random()
+        val colorNum = random.nextInt(2)
+        val avatarNum = random.nextInt(28)
 
+        userAvatar = if (colorNum == 0) {
+            "light$avatarNum"
+        } else {
+            "dark$avatarNum"
+        }
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        signupAvatarPreview.setImageResource(resourceId)
     }
 
     fun onAvatarClicked(view: View){
