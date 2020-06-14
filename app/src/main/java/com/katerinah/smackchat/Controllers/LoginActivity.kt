35e8ce6806a7bf8376b5283ff76b4c4419a08b0c
Katerina Hanson <katerina.hanson@gmail.com>
2020-06-14 01:11:49 -0700

    Fix

--- app/src/main/java/com/katerinah/smackchat/Controllers/LoginActivity.kt
+++ app/src/main/java/com/katerinah/smackchat/Controllers/LoginActivity.kt
@@ -45,7 +45,7 @@ class LoginActivity : AppCompatActivity() {
     fun hideKeyboard() {
         val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         if (inputManager.isAcceptingText) {
-            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
+            inputManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         }
     }
 
